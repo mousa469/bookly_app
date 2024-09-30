92bc0a1a633bdb762bcb17d40f079d71e6efe283
mousa469 <mohamed.mouza469@gmail.com>
2024-09-30 19:28:44 +0300

    create book details  view

---
+++ lib/features/home/presentation/views/widgets/book_details_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class BookDetailsBody extends StatelessWidget {
+  const BookDetailsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Column(
+      children: [
+
+      ],
+    );
+  }
+}
