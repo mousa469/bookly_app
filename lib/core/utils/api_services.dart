dedf04e00bf00110b91bfbdb896c5d8b63a8e9dd
mousa469 <mohamed.mouza469@gmail.com>
2024-10-04 09:59:13 +0300

    implement fetch best seller books

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -3,10 +3,10 @@ import 'package:dio/dio.dart';
 class ApiServices {
   final _baseUrl = "https://www.googleapis.com/books/v1/";
   final _apiKey = "AIzaSyDiE6OnQQYkKlLUhjChYMwtVFGqAZDSUWw";
-  Dio dio = Dio();
+  Dio _dio = Dio();
 
   Future<Map<String, dynamic>>  get({required String endPoint}) async {
-    Response response = await dio.get("$_baseUrl$endPoint");
+    Response response = await _dio.get("$_baseUrl$endPoint$_apiKey");
     return response.data;
   }
 }
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,13 +1,28 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utils/api_services.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImplementation  implements HomeRepo {
+class HomeRepoImplementation implements HomeRepo {
+  ApiServices apiServices;
+
+  HomeRepoImplementation({required this.apiServices});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      List<BookModel> books = [];
+      var data = await apiServices.get(
+          endPoint:
+              "volumes?q=subject:programming&Filtering=free-ebooks&sorting=newest");
+
+      for (var book in data["items"]) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return Left(ServerFailure());
+    }
   }
 
   @override
@@ -15,5 +30,4 @@ class HomeRepoImplementation  implements HomeRepo {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
-  
 }
