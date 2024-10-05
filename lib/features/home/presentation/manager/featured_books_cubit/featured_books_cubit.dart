f689772eb8fe09535d18d5a9ccbaa15881ba0196
mousa469 <mohamed.mouza469@gmail.com>
2024-10-05 09:32:17 +0300

    create featured books cubit methods

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,16 +1,22 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo_implementation.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksStates> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitialState());
+  FeaturedBooksCubit(this.homeRepoImplementation)
+      : super(FeaturedBooksInitialState());
+
+  final HomeRepoImplementation homeRepoImplementation;
 
   fetchFeaturedBooks() async {
     emit(FeaturedBooksLoadingState());
-    try {
-     List<BookModel> books =  await HomeRepoImplementation().fetchFeaturedBooks();
-      emit(FeaturedBooksSuccessState(booksList: ));
-    } catch (e) {}
+    var result = await homeRepoImplementation.fetchBestSellerBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailureState(errMessage: failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccessState(booksList: books));
+    });
   }
 }
