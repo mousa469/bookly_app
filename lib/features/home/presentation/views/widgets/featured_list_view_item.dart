be52050f5b8fb3eb115094d15c87bbf07a840436
mousa469 <mohamed.mouza469@gmail.com>
2024-10-06 09:04:23 +0300

    cached network image

--- lib/features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,22 +1,21 @@
-import 'package:bookly/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedListViewItem extends StatelessWidget {
   const FeaturedListViewItem({super.key, required this.imgUrl});
 
-  final  String imgUrl;
+  final String imgUrl;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.4 / 2,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imgUrl),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 1.4 / 2,
+        child: CachedNetworkImage(
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          imageUrl: imgUrl,
+          fit: BoxFit.fill,
         ),
       ),
     );
